2cd99feffa1a6640b9bf197d8b5a78ca7c3424ca
Hridoy <rkhridoy68@gmail.com>
2021-04-09 14:39:24 +0600

    added main.js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// function stickyScroll()
+// {
+//     window.addEventListener("scroll",function()
+//     {
+//         var header = document.querySelector("navigation");
+//         header.classList.toggle("sticky",window.scrollY>0);
+//     })
+// }
