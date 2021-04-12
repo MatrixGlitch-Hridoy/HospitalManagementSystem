c53145ba212365e7b1e983af094103ae5ba65d0d
Hridoy <rkhridoy68@gmail.com>
2021-04-12 23:48:26 +0600

    cookie updated

--- controls/cookies.php
+++ controls/cookies.php
@@ -1,5 +1,5 @@
 <?php
     $cookie_name = "users";
     $cookie_value = "username";
-    setcookie($cookie_name,$cookie_value,time()+(86400+30),"/");
+    setcookie($cookie_name,$cookie_value,time()+(86400*30),"/");
 ?>
