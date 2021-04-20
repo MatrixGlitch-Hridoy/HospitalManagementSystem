ba52dd3f24de5298c768969d2290948a7fe353d6
Hridoy <rkhridoy68@gmail.com>
2021-04-21 00:45:50 +0600

    some changes made

--- patient/dashboard.php
+++ patient/dashboard.php
@@ -41,7 +41,7 @@
         <nav class="menu">
           <ul>
             <li>
-             <a href=""><?php echo $_SESSION['username'];?></a>
+             <a href="dashboard.php"><?php echo $_SESSION['username'];?></a>
               <ul>
                 <li><a href="../controls/logout.php">Logout</a></li>
               </ul>
@@ -50,7 +50,7 @@
         </nav>
       </div>
     </header>
-
+      <?php $uid = $_SESSION['id']; ?>
     <!-- Admin Page Wrapper -->
     <div class="admin-wrapper">
       <!-- Left Sidebar -->
@@ -58,7 +58,7 @@
         <ul>
           <li><a href="filter.php">Book Apointment</a></li>
           <li><a href="appointment-history.php">Apointment History</a></li>
-          <li><a href="update-profile.php">Update Profile</a></li>
+          <li><a href="update-profile.php?editid=<?php echo $uid; ?>">Update Profile</a></li>
         </ul>
       </div>
       <!-- // Left Sidebar -->
